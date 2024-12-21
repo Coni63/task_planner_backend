83d9932b3cd0564c6b4074a980a0536a9ae2de45
Nicolas M <mine.nicolas@gmail.com>
2024-12-21 22:32:36 +0100

    start implementing rules

--- task_manager/serializers/task_serializer.py
+++ task_manager/serializers/task_serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from task_manager.models import Task, TaskAudit
+from task_manager.models import Status, Task, TaskAudit
 from task_manager.serializers.base_serializer import BaseSerializer
 from task_manager.serializers.category_serializer import CategorySerializer
 from task_manager.serializers.project_serializer import ProjectSerializer
@@ -34,6 +34,30 @@ class TaskSimpleSerializer(BaseSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+    def validate(self, data):
+        # check default constraints
+        data = super().validate(data)
+
+        # check custom constraints
+        self.__check_task_not_closed(data)
+        self.__check_active_state_without_picked_by(data)
+
+        return data
+    
+    def __check_task_not_closed(self, data):
+        # Even an admin cannot modified a closed task
+        task = self.instance
+        if task and task.status.state == "closed":
+            raise serializers.ValidationError("Cannot update a closed task")
+
+    def __check_active_state_without_picked_by(self, data):
+        # Cannot set status to active or blocked without a user assigned
+        status = data.get("status") or (self.instance.status if self.instance else None)
+        picked_by = data.get("picked_by") or (self.instance.picked_by if self.instance else None)
+
+        if status and status.state in ["active", "blocked", "closed"] and picked_by is None:
+            raise serializers.ValidationError("Cannot set status to active, blocked, or closed without a user assigned")
 
 
 class TaskAuditSerializer(BaseSerializer):
